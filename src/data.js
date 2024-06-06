6ef7d2af9661619e96729fb7cf3cb07ec4460656
mnyaxx <myranyakiamo11@gmail.com>
2024-06-06 16:43:02 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+let shopItemsData = [{
+    id:"yeeeh",
+    name:"Casual Shirt",
+    price: 35,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam deserunt nam error earum, ad aliquid voluptates nobis. Sunt consequatur voluptatem esse natus facilis magni molestiae fuga? Maxime beatae libero quibusdam",
+    img: "images/img-1.jpg"
+},
+{
+    id:"yaaah",
+    name:"Office Shirt",
+    price: 95,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam deserunt nam error earum, ad aliquid voluptates nobis. Sunt consequatur voluptatem esse natus facilis magni molestiae fuga? Maxime beatae libero quibusdam",
+    img: "images/img-2.jpg"
+},
+{
+    id:"yiiih",
+    name:"T Shirt",
+    price: 15,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam deserunt nam error earum, ad aliquid voluptates nobis. Sunt consequatur voluptatem esse natus facilis magni molestiae fuga? Maxime beatae libero quibusdam",
+    img: "images/img-5.jpg"
+},
+{
+    id:"yoooh",
+    name:"Mens Suit", 
+    price: 285,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam deserunt nam error earum, ad aliquid voluptates nobis. Sunt consequatur voluptatem esse natus facilis magni molestiae fuga? Maxime beatae libero quibusdam",
+    img: "images/img-4.jpg"
+}]
